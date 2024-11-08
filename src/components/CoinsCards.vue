017857a913a0ac22bfa9c61ac207b7134594de8a
Kim_Arthem <sonkimarthem@gmail.com>
2024-11-09 00:02:41 +0800

    'bugfix_hover'

--- src/components/CoinsCards.vue
+++ src/components/CoinsCards.vue
@@ -5,7 +5,7 @@ import { ref } from 'vue';
 const props = defineProps({
     data: {
       type: Object,
-      default: []
+      default: {}
     }
 })
 
@@ -22,7 +22,7 @@ function ShowCoinInfo(){
 
 <template>
       <CoinInfo :data="props.data" v-if="toggleInfo" :toggleInfo="ShowCoinInfo"/> 
-      <div class="flex items-center justify-center text-[white] border-2 border-white h-[100px] w-[200px] bg-[rgb(50, 62, 110, 1.3)] cursor-pointer
+      <div v-else class="flex items-center justify-center text-[white] border-2 border-white h-[100px] w-[200px] bg-[rgb(50, 62, 110, 1.3)] cursor-pointer
       hover:shadow-[-2px_2px_2px_rgb(29,29,29)] hover:brightness-[1.1] bg-[#323e6e]"
       @click="ShowCoinInfo2">
         <img v-bind:src="props.data.image" alt="Coin" class="w-[50px] h-[50px] mr-[5px]">
